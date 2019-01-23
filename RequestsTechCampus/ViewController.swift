882af88b0fc31eb825989b60d4ace7c2ae45d5b0
TechCampus <techcampus@razans-MacBook-Pro.local>
2019-01-24 00:20:32 +0200

    Request Practice

--- RequestsTechCampus/ViewController.swift
+++ RequestsTechCampus/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+      let imageUrl = URL(string: "https://techcampus.com/uploads/c983e6c92f62ac2368f51e0a462556ba.jpg")!
+        
+        //doesn't happen automatically, needs resume()
+        let task = URLSession.shared.dataTask(with: imageUrl) { (data, response, error) in
+            if error == nil {
+                let loadedImage = UIImage(data: data!)
+                DispatchQueue.main.async {
+                    self.imageView.image = loadedImage
+                }
+            }
+        }
+        
+        task.resume()//call the request, without that line request wouldn't be called
     }
 
 
